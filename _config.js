f7f6c51442a94191d36cf5f5b3acd7f657428c6a
Abdihakim Muhumed <abdihakim.0017@gmail.com>
2022-11-23 22:14:51 +0300

    Procfile editing

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password -- ie uri for mongodb atlas
 config.mongoURI = {
-    production: 'mongodb+srv://abdihakim-muhumed:kWYjKQg6AgSFD1Uv@cluster0.lj8fmwo.mongodb.net/?retryWrites=true&w=majority',
-    development: 'mongodb+srv://abdihakim-muhumed:kWYjKQg6AgSFD1Uv@cluster0.lj8fmwo.mongodb.net/?retryWrites=true&w=majority',
-    test: 'mongodb+srv://abdihakim-muhumed:kWYjKQg6AgSFD1Uv@cluster0.lj8fmwo.mongodb.net/?retryWrites=true&w=majority',
+    production: 'mongodb+srv://abdihakim-muhumed:sQM3fAIrQloziTIw@gallery-1-0.20kru4h.mongodb.net/?retryWrites=true&w=majority',
+    development: 'mongodb+srv://abdihakim-muhumed:sQM3fAIrQloziTIw@gallery-1-0.20kru4h.mongodb.net/?retryWrites=true&w=majority',
+    test: 'mongodb+srv://abdihakim-muhumed:sQM3fAIrQloziTIw@gallery-1-0.20kru4h.mongodb.net/?retryWrites=true&w=majority',
 }
 module.exports = config;
