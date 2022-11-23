4ba4a68c8f051508741b52384c372cf772f017e7
Abdihakim Muhumed <abdihakim.0017@gmail.com>
2022-11-23 19:33:39 +0300

    Milestone -1

--- _config.js
+++ _config.js
@@ -1,6 +1,6 @@
 var config = {}
 
-// Update to have your correct username and password -- ie uri
+// Update to have your correct username and password -- ie uri for mongodb atlas
 config.mongoURI = {
     production: 'mongodb+srv://abdihakim-muhumed:kWYjKQg6AgSFD1Uv@cluster0.lj8fmwo.mongodb.net/?retryWrites=true&w=majority',
     development: 'mongodb+srv://abdihakim-muhumed:kWYjKQg6AgSFD1Uv@cluster0.lj8fmwo.mongodb.net/?retryWrites=true&w=majority',
