a0365315b6ce54000705cb000f0d3e6b2d112b81
Abdihakim Muhumed <abdihakim.0017@gmail.com>
2022-11-24 10:12:53 +0300

    Milestone-1

--- _config.js
+++ _config.js
@@ -1,6 +1,6 @@
 var config = {}
 
-// Update to have your correct username and password -- ie uri for mongodb atlas
+// Update to have your correct username and password -- ie uri for mongodb atlas -
 config.mongoURI = {
     production: 'mongodb+srv://abdihakim-muhumed:sQM3fAIrQloziTIw@gallery-1-0.20kru4h.mongodb.net/darkroom?retryWrites=true&w=majority',
     development: 'mongodb+srv://abdihakim-muhumed:sQM3fAIrQloziTIw@gallery-1-0.20kru4h.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
