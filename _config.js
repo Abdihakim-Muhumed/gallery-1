e971321627822e53e72b5f8ef599d4a1b6686e22
Abdihakim Muhumed <abdihakim.0017@gmail.com>
2022-11-25 12:02:59 +0300

    Milestone-4

--- _config.js
+++ _config.js
@@ -4,6 +4,6 @@ var config = {}
 config.mongoURI = {
     production: 'mongodb+srv://abdihakim-muhumed:sQM3fAIrQloziTIw@gallery-1-0.20kru4h.mongodb.net/darkroom?retryWrites=true&w=majority',
     development: 'mongodb+srv://abdihakim-muhumed:sQM3fAIrQloziTIw@gallery-1-0.20kru4h.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://abdihakim-muhumed:sQM3fAIrQloziTIw@gallery-1-0.20kru4h.mongodb.net/darkroom?retryWrites=true&w=majority',
+    test: 'mongodb+srv://abdihakim-muhumed:sQM3fAIrQloziTIw@gallery-1-0.20kru4h.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
 module.exports = config;
